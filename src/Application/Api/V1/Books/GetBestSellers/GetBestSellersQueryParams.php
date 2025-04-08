76813746df017d6cc2c9443e5b9ffc94a9487192
David Oury <oury.david@gmail.com>
2025-04-08 16:44:26 +0200

    refactor: split routes by api version and resources

--- src/Application/Api/V1/Books/GetBestSellers/GetBestSellersQueryParams.php
+++ src/Application/Api/V1/Books/GetBestSellers/GetBestSellersQueryParams.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Application\Api\V1\GetBestSellers;
+namespace App\Application\Api\V1\Books\GetBestSellers;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
